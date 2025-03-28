35ba10fd8ef35a25cc14fa46ddaea77878a05594
Büşra Atasoy <bsraklcsln@gmail.com>
2025-03-28 13:19:45 +0300

    Create band_name_generator.py

---
+++ 1-Band Name Generator/band_name_generator.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator! 🎶🎸")
+
+# City input from user
+city = input("What's the name of the city you grew up in?\n")
+
+# Pet input from user
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be: " + city + " " + pet)
